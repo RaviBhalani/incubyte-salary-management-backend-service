d4b2c1db95fced49a75e3e09db8b9c1cb0541766
RaviBhalani <ravibhalani8@gmail.com>
2026-04-06 01:25:45 +0530

    feature/basic-setup
    [-] Remove unused constants.

--- apps/user/constants.py
+++ apps/user/constants.py
@@ -10,38 +10,13 @@ App constants end.
 
 
 """
-Constants for TokenObtainPair starts
+Message constants start.
 """
 
 USER_DOES_NOT_EXIST = "User Does not exists"
 NO_ACTIVE_ACCOUNT_FOUND = "No active account found with the given credentials"
+PASSWORDS_DO_NOT_MATCH = "Passwords don't match"
 
 """
-Constants for TokenObtainPair ends
-"""
-
-
-"""
-Constant for User Model starts
-"""
-
-FIRST_NAME_USER_HELP_TEXT = "First name of the user"
-MIDDLE_NAME_USER_HELP_TEXT = "Middle name of the user"
-LAST_NAME_USER_HELP_TEXT = "Last name of the user"
-EMAIL_HELP_TEXT = "Email ID of user"
-VERIFIED_HELP_TEXT = "User verification status"
-
-"""
-Constant for User Model ends
-"""
-
-
-"""
-Constants for field lengths starts
-"""
-
-DEFAULT_NAME_FIELD_MAX_LENGTH = 150
-
-"""
-Constants for field lengths ends
+Message constants end.
 """
