6440c7c9ec80c1ccacf3f9fbcbf8adcd5747ddec
RaviBhalani <ravibhalani8@gmail.com>
2026-04-05 20:54:07 +0530

    feature/basic-setup
    [*] Make cosmetic changes in the constants.py for better readability.

--- incubyte_salary_management_backend_service/constants.py
+++ incubyte_salary_management_backend_service/constants.py
@@ -1,11 +1,22 @@
 from apps.core.url_builder import build_url
 
+
+"""
+URLs start.
+"""
+
+V1_API_PREFIX = "api/v1"
+
 ADMIN_URL = "admin/"
 
-V1_API_PREFIX = "api/v1/"
-
-V1_SCHEMA_URL = build_url(V1_API_PREFIX, "schema/")
-V1_DOCS_URL = build_url(V1_API_PREFIX, "docs/")
-
+SCHEMA_URL = "schema/"
+V1_SCHEMA_URL = build_url(V1_API_PREFIX, SCHEMA_URL)
 SCHEMA_V1_NAME = "schema-v1"
+
+DOCS_URL = "docs/"
+V1_DOCS_URL = build_url(V1_API_PREFIX, DOCS_URL)
 SWAGGER_UI_V1_NAME = "swagger-ui-v1"
+
+"""
+URLs end.
+"""
