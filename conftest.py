e7286c0ffb85dd4a67e3655bffc17d2d0581043e
RaviBhalani <ravibhalani8@gmail.com>
2026-04-07 18:54:45 +0530

    feature/employee-crud
    [+] Add common authenticated_client for use in all endpoints going ahead.

--- conftest.py
+++ conftest.py
@@ -1,7 +1,16 @@
 import pytest
 from rest_framework.test import APIClient
 
+from tests.factories.user import create_user
+
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+@pytest.fixture
+def authenticated_client(api_client):
+    user = create_user()
+    api_client.force_authenticate(user=user)
+    return api_client
