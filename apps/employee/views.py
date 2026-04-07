4bc0f6bef4d79085c53cd4f52f4ba30b92d2beac
RaviBhalani <ravibhalani8@gmail.com>
2026-04-07 23:10:56 +0530

    feature/employee-crud
    [*] Restrict delete from the view set as it is no longer required due to soft deletion.

--- apps/employee/views.py
+++ apps/employee/views.py
@@ -1,8 +1,10 @@
+from apps.core.constants import GET, PATCH, POST
 from apps.core.views import BaseViewset
 from apps.employee.models import Employee
 from apps.employee.serializers import EmployeeSerializer
 
 
 class EmployeeViewset(BaseViewset):
+    http_method_names = [GET, POST, PATCH]
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
