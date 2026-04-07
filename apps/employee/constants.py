c1088c2eabac8e8f3ea822f701ee3713d6c08911
RaviBhalani <ravibhalani8@gmail.com>
2026-04-07 18:27:30 +0530

    feature/employee-crud
    [+] Add constants for the model, app and urls.

---
+++ apps/employee/constants.py
@@ -0,0 +1,38 @@
+"""
+App name start.
+"""
+
+EMPLOYEE_APP = "apps.employee"
+
+"""
+App name end.
+"""
+
+
+"""
+URL constants start.
+"""
+
+EMPLOYEE_URL = "employee"
+EMPLOYEE_BASENAME = "employee"
+EMPLOYEE_LIST_NAME = "employee-list"
+EMPLOYEE_DETAIL_NAME = "employee-detail"
+
+"""
+URL constants end.
+"""
+
+
+"""
+Model constants start.
+"""
+
+EMPLOYEE_ID_MAX_LENGTH = 13
+EMPLOYEE_NAME_MAX_LENGTH = 300
+EMPLOYEE_JOB_TITLE_MAX_LENGTH = 150
+EMPLOYEE_DEPARTMENT_MAX_LENGTH = 150
+EMPLOYEE_COUNTRY_MAX_LENGTH = 150
+
+"""
+Model constants end.
+"""
