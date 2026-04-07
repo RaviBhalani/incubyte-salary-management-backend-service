c5fc6f95163233a80a8a1dc83ad788882f3455ae
RaviBhalani <ravibhalani8@gmail.com>
2026-04-07 21:16:49 +0530

    feature/employee-crud
    [+] Add failing test for job title.

--- apps/employee/tests/api/test_create_employee_api.py
+++ apps/employee/tests/api/test_create_employee_api.py
@@ -1,8 +1,9 @@
 import pytest
 from rest_framework import status
 
-from apps.employee.tests.constants import DATA_KEY, EMAIL_FIELD, EMPLOYEE_ID_FIELD
+from apps.employee.tests.constants import DATA_KEY, EMAIL_FIELD, EMPLOYEE_ID_FIELD, INVALID_JOB_TITLE, JOB_TITLE_FIELD
 from tests.constants import JSON_FORMAT
+from tests.helpers.assertions import assert_error_response
 
 pytestmark = pytest.mark.django_db
 
@@ -15,3 +16,9 @@ class TestCreateEmployeeApi:
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data[DATA_KEY][EMPLOYEE_ID_FIELD] == employee_payload[EMPLOYEE_ID_FIELD]
         assert response.data[DATA_KEY][EMAIL_FIELD] == employee_payload[EMAIL_FIELD]
+
+    def test_returns_error_for_invalid_job_title(self, authenticated_client, employee_url, employee_payload):
+        employee_payload[JOB_TITLE_FIELD] = INVALID_JOB_TITLE
+        response = authenticated_client.post(employee_url, employee_payload, format=JSON_FORMAT)
+
+        assert_error_response(response, status.HTTP_400_BAD_REQUEST)
