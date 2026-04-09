c5d7f1599e8161db9a752313c788e3516fd8ab81
RaviBhalani <ravibhalani8@gmail.com>
2026-04-09 22:58:08 +0530

    feature/seed-management-command
    [+] Add max employee number generation to model as class method as it will be used by employee seeding management command as well.

--- apps/employee/models.py
+++ apps/employee/models.py
@@ -1,11 +1,14 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models import IntegerField, Max
+from django.db.models.functions import Cast, Substr
 
 from apps.core.models import IsActive
 from apps.employee.constants import (
     EMPLOYEE_COUNTRY_MAX_LENGTH,
     EMPLOYEE_DEPARTMENT_MAX_LENGTH,
     EMPLOYEE_ID_MAX_LENGTH,
+    EMPLOYEE_ID_NUMERIC_START_INDEX,
     EMPLOYEE_JOB_TITLE_MAX_LENGTH,
     EMPLOYEE_NAME_MAX_LENGTH,
     EMPLOYEE_SALARY_MAX_VALUE,
@@ -32,6 +35,12 @@ class Employee(IsActive):
 
     class Meta:
         db_table = "employee"
+
+    @classmethod
+    def get_max_employee_number(cls):
+        return cls.objects.aggregate(
+            max_num=Max(Cast(Substr("employee_id", EMPLOYEE_ID_NUMERIC_START_INDEX), IntegerField()))
+        )["max_num"] or 0
 
     def __str__(self):
         return self.name
