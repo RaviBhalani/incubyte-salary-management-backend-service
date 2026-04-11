1c30e2c3c2b28da9fcecbd87f1f03d98b9cb71c8
RaviBhalani <ravibhalani8@gmail.com>
2026-04-11 19:05:48 +0530

    feature/token-blacklist
    [+] Add token blacklist endpoint for logout purpose.

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenBlacklistView, TokenObtainPairView, TokenRefreshView
 
 from apps.user.constants import (
     LOGIN_NAME,
     LOGIN_URL,
+    LOGOUT_NAME,
+    LOGOUT_URL,
     TOKEN_REFRESH_NAME,
     TOKEN_REFRESH_URL,
 )
@@ -15,5 +17,10 @@ urlpatterns = [
         TOKEN_REFRESH_URL,
         TokenRefreshView.as_view(),
         name=TOKEN_REFRESH_NAME,
+    ),
+    path(
+        LOGOUT_URL,
+        TokenBlacklistView.as_view(),
+        name=LOGOUT_NAME,
     ),
 ]
